a5dedc09a075dcb69642bc5805df0c7d99036b7e
Andreas Lind <andreaslindpetersen@gmail.com>
2019-03-16 23:15:01 +0100

    Browser: Pass on extra options

--- index.browser.js
+++ index.browser.js
@@ -1,9 +1,10 @@
 const fontTracer = require('./lib/fontTracer');
 const gatherStylesheetsWithPredicates = require('./lib/gatherStylesheetsWithPredicates');
 
-module.exports = function(document) {
+module.exports = function(document, options) {
   return fontTracer(document, {
     stylesheetsWithPredicates: gatherStylesheetsWithPredicates(document),
-    getCssRulesByProperty: require('assetgraph/lib/util/fonts/getCssRulesByProperty')
+    getCssRulesByProperty: require('assetgraph/lib/util/fonts/getCssRulesByProperty'),
+    ...options
   });
 };
